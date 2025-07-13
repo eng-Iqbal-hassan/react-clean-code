2377280eb1955cfc619cd8c23b88f36efd7267a6
Iqbal Hassan <iqbal.hassan@cogentlabs.co>
2025-07-14 00:14:14 +0500

    data is obtained

--- src/pages/post/components/Post.jsx
+++ src/pages/post/components/Post.jsx
@@ -6,9 +6,22 @@ export function Post() {
     queryKey: ['post'],
     queryFn: async () => {
       const response = await axios.get('https://jsonplaceholder.typicode.com/posts');
-
       return Object.values(response.data);
     },
   });
-  return <div>{postData.isSuccess && postData.data.map((value, index) => <div key={index}>{value.title}</div>)}</div>;
+
+  return (
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-5 gap-5 p-5">
+      {postData.isSuccess &&
+        postData.data.map((value, index) => (
+          <div
+            key={index}
+            className="bg-white shadow-md border border-gray-200 rounded-lg p-4 hover:shadow-lg transition-shadow"
+          >
+            <h2 className="text-lg font-semibold mb-2">{value.title}</h2>
+            <p className="text-gray-600">{value.body}</p>
+          </div>
+        ))}
+    </div>
+  );
 }
